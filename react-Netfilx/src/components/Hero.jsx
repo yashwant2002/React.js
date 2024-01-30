9514690464454f79c1faf7fe9129fa8233f66734
yashwant2002 <yashsahu3333@gmail.com>
2024-01-30 06:50:06 +0530

    react-Netflix-clone

---
+++ react-Netfilx/src/components/Hero.jsx
@@ -0,0 +1,52 @@
+
+import React, {useEffect, useState} from 'react'
+import axios from "axios"
+import endpoints from '../service/movieService.js';
+
+function hero() {
+    const [movie, setMovie] = useState([])
+    useEffect(()=>{
+      axios.get(endpoints.popular).then((res) => {
+          const movies = res.data.results;
+          const randomMovie = movies[Math.floor(Math.random() * movies.length)]
+          setMovie(randomMovie);
+      })
+    },[]) 
+    console.log(movie)
+    if(!movie)
+      return(
+          <>
+              <p>fetching movie....</p>
+          </>
+          );
+  
+  const {title, backdrop_path, release_date, overview} = movie;
+    return (
+      <div className='w-full h-[550px] lg:h-[850px] text-white'>
+        <div className='w-full h-full '>
+            <div className=' absolute w-full h-[550px] lg:h-[850px] bg-gradient-to-r from-black'></div>
+            <img className=' w-full h-full object-cover object-top' src={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt={`${title}`}/>
+        </div>
+        <div className='absolute w-full top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+          <div className='my-4'>
+            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
+              Play
+            </button>
+            <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
+              Watch Later
+            </button>
+          </div>
+          <p className='text-gray-400 text-sm'>
+            Released: {movie?.release_date}
+          </p>
+          <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
+            {overview}
+          </p>
+        </div>
+      </div>
+
+    );
+}
+
+export default hero
---
+++ react-Netfilx/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className=' absolute w-full p-4 flex justify-between items-center z-50'>
+        <Link to={'/'}>
+            <h1 className='uppercase text-red-600 font-nsans-bold cursor-pointer text-5xl'>netfilx</h1>
+        </Link>
+        <div>
+            <Link to={'/login'}>
+                <button className=' capitalize pr-5' >Login</button>
+            </Link>
+            <Link to={'/signup'}>
+                <button className='capitalize py-2 px-6 rounded-md cursor-pointer bg-red-600 hover:bg-red-500'>signup</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
