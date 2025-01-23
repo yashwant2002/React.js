383feaaaa7a98073941f14971dcdfb7b626ff23f
yashwant2002 <yashsahu3333@gmail.com>
2025-01-23 19:25:01 +0530

    counter app

---
+++ 01-Counter_app/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+function App() {
+  const [count,setCount] = useState(0)
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App
